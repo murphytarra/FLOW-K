81822eb956656ec858c2a86091552443cbd54d80
murphytarra <68056855+murphytarra@users.noreply.github.com>
2022-03-18 21:03:25 +0000

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+from numpy import linalg as LA
+import matplotlib.pyplot as plt
+from numpy.linalg import multi_dot
+from numpy.linalg import inv
+import math
+import cmath
+
+from Electrons import Electrons
+from E_field import E_field
+from coupling2 import coupling2
